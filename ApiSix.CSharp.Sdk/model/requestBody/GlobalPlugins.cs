fc3f315c4bd1e385ef59566e734ba3ea4b28dd1e
stoneson <stoneson.xie@qq.com>
2023-07-14 17:44:48 +0800

    add GlobalPlugins

--- ApiSix.CSharp.Sdk/model/requestBody/GlobalPlugins.cs
+++ ApiSix.CSharp.Sdk/model/requestBody/GlobalPlugins.cs
@@ -4,10 +4,7 @@ using System.Collections.Generic;
 
 namespace ApiSix.CSharp.model
 {
-    /// <summary>
-    /// Global Rule 可以设置全局运行的插件，设置为全局规则的插件将在所有路由级别的插件之前优先运行
-    /// </summary>
-    public class GlobalRule : BaseBody
+    public class GlobalPlugins : BaseBody
     {
         /// <summary>
         /// Plugin 配置
